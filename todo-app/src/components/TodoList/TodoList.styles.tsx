71baca2657ad6b9cb72d1713a1c123ee306b5305
SIRU TAN <siru0915@gmail.com>
2023-03-23 08:22:43 -0400

    Reset function done + working on adding items.

--- todo-app/src/components/TodoList/TodoList.styles.tsx
+++ todo-app/src/components/TodoList/TodoList.styles.tsx
@@ -16,7 +16,6 @@ export const Content = styled.div`
 export const Modify = styled.div`
     width:100%;
     height:80px;
-    /* background-color: red; */
     display:flex;
     flex-direction: row;
     justify-content: center; 
@@ -48,6 +47,21 @@ export const Refresh = styled.button`
     font-weight: bold;
 `;
 
+export const Reset = styled.button`
+    width:70px;
+    height:50px;
+    background-color: white;
+    font-weight: bold;
+    font-size: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+    margin-left: 20px;
+    font-weight: bold;
+`;
+
 export const Add = styled.button`
     width:70px;
     height:50px;
@@ -60,5 +74,48 @@ export const Add = styled.button`
     justify-content: center;
     border-radius: 10px;
     font-weight: bold;
-
 `;
+
+export const Empty = styled.div`
+    width:100%;
+    height:100px;
+    color:black;
+    font-size:28px;
+    font-weight: bold;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Form = styled.form`
+    width:500px;
+    height:300px;
+    align-self: center;
+`;
+
+
+export const Submit = styled.input.attrs({type:"submit"})`
+    height:25px;
+    width:80px;
+    border-radius: 15px;
+    border: 1px solid black;
+    margin-left: auto;
+    margin-right: 10px;
+    `;
+
+
+export const Limit = styled.div`
+    font-size: 20px;
+    font-weight: bold;
+    color:red;
+`;
+
+export const NewTask = styled.textarea`
+    width:500px;
+    height:50px;
+    font-size:30px;
+    :focus{
+        outline:none;
+    }
+    `;
--- todo-app/src/components/TodoList/TodoList.tsx
+++ todo-app/src/components/TodoList/TodoList.tsx
@@ -1,6 +1,7 @@
 import TodoItem from '../TodoItem/TodoItem'
-import { Add, Content, Modify, Refresh, TaskList, Wrapper } from './TodoList.styles'
+import { Add, Content, Empty, Modify, Refresh, Reset, TaskList, Wrapper, Form, Submit, Limit,  NewTask} from './TodoList.styles'
 import {useState} from 'react';
+import Modal from '../Modal/Modal';
 var randomSentence = require('random-sentence');
 
 var data = [
@@ -117,10 +118,22 @@ var data = [
 
 export default function TodoList() {
     const [tasks,setTasks] = useState(data)
+    const [isOpen,setIsOpen] = useState(false);
+
+    const limit = 20;
+    // const check = text.length === limit ? true : false;
+
+    function handleChange(event:any) {
+    }
 
 
-    function timeout(delay: number) {
-        return new Promise( res => setTimeout(res, delay) );
+    function onClose(){
+        setIsOpen(false);
+    }
+
+    function onSubmit(event:any){
+        event.preventDefault()
+        setIsOpen(false);
     }
 
     async function handleDelete(key:any){
@@ -131,21 +144,41 @@ export default function TodoList() {
         setTasks(data)
     }
 
+    function doReset(){
+        setTasks([])
+    }
+
+    function addItem(){
+
+    }
+
     return (
     <Wrapper>
         <Content>
             <Modify>
-                <Add>
+                <Add onClick={() => setIsOpen(true)}>
                     Add Item
                 </Add>
                 <Refresh onClick={doRefresh}>
                     Refresh
                 </Refresh>
+                <Reset onClick={doReset}>
+                    Reset
+                </Reset>
             </Modify>
             <TaskList>
                 {tasks.map((task,_)=><TodoItem key={task.task} task={task.task} details={task.details} handleDelete={handleDelete} />)}
-                
+                {tasks.length === 0 && <Empty>Currently no tasks are recorded!</Empty>}
             </TaskList>
+            <Modal open={isOpen} onClose={onClose} handleChange={handleChange}  limit={limit} onSubmit={onSubmit}>
+            What would you like to change the title to?
+
+            <Form onSubmit={onSubmit}>
+                <NewTask/>
+                {/* {check && <Limit>Max limit is {limit} chars!</Limit>} */}
+                <Submit/>
+            </Form>
+        </Modal>
         </Content>
     </Wrapper>
     )
